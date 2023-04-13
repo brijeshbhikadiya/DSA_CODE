fb617f92cca90d02dbc53f0baaa51edc1214d638
BRIJESH BHIKADIYA <89252901+brijeshbhikadiya@users.noreply.github.com>
2023-04-13 00:55:12 -0600

    Update binary to decimal.cpp

--- LogicQuestion/binary to decimal.cpp
+++ LogicQuestion/binary to decimal.cpp
@@ -12,14 +12,14 @@ int main()
     int n;
     cin>>n;
 
-    int ans=0;
+    int ans1=0;
     int i=0;
     while(n!=0)
     {
         int bit=n%10;
-        ans=(bit*pow(2,i))+ans;
+        ans1=(bit*pow(2,i))+ans1;
         n=n/10;
         i++;
     }
-    cout<<ans<<endl;
+    cout<<ans1<<endl;
 }
