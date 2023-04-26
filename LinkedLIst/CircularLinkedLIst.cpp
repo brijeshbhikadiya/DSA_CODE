19b9e4549e998107d9a266d5072da7223d85539b
brijeshbhikadiya <brijeshbhikadiya27@gmail.com>
2023-04-26 11:17:32 -0600

    circular linklist code added

---
+++ LinkedLIst/CircularLinkedLIst.cpp
@@ -0,0 +1,121 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+
+    int data;
+    Node* next;
+
+    Node(int data)
+    {
+        this->data=data;
+        this->next=NULL;
+    }
+      
+      ~Node()
+    {
+        int value=this->data;
+        if(this->next!=NULL)
+        {
+            delete next;
+            next=NULL;
+        }
+        cout<<"memory free for "<<value<<endl;
+    }
+};
+void print(Node* &tail)
+{
+   
+    Node* temp=tail;
+    do
+    {
+        cout<<tail->data<<" ";
+        tail=tail->next;
+    } while (tail!=temp);
+    cout<<endl;
+    
+}
+
+void insertElement(Node* &tail,int element,int d)
+{
+
+   
+   
+    //empty list
+    if(tail==NULL)
+    {
+       
+        Node* newnode=new Node(d);
+        tail=newnode;
+        newnode->next=newnode;
+    }
+    else
+    {
+
+        //non-empty list
+        //assuring that the element is presenting.
+        Node* curr=tail;
+
+        while(curr->data!=element)
+        {
+            curr=curr->next;
+        }
+
+        //element found current represting current vala element.
+        Node* temp=new Node(d);
+        temp->next=curr->next;
+        curr->next=temp;
+    }
+}
+
+void deleteNode(Node* &tail,int value)
+{
+    if(tail==NULL)
+    {
+        cout<<"LINKEDLIST IS EMPTY PLEAS ENTER A VALUE"<<endl;
+        return;
+    }
+    else{
+        Node* prev=tail;
+        Node* curr=prev->next;
+
+        while(curr->data!=value)
+        {
+            prev=curr;
+            curr=curr->next;
+        }
+
+        prev->next=curr->next;
+        if(tail==curr)
+        {
+            tail=prev;
+        }
+        curr->next=NULL;
+        delete curr;
+    }
+}
+
+int main()
+{
+    Node* tail=NULL;
+
+
+
+    insertElement(tail,5,3);
+    print(tail);
+
+    insertElement(tail,3,7);
+    print(tail);
+
+    insertElement(tail,7,9);
+    print(tail);
+
+    insertElement(tail,9,11);
+    print(tail);
+
+    insertElement(tail,7,12);
+    print(tail);
+
+    deleteNode(tail,12);
+    print(tail);
+}
