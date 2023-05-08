3b14490db8f16599aa486eba8b577e1209d07da4
brijeshbhikadiya <brijeshbhikadiya27@gmail.com>
2023-05-08 10:26:21 -0600

    added 2d array question

---
+++ dynamic memo/leetcodeSumOfDiagonal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+
+        int n=mat.size();
+        int sum=0;
+
+        for(int i=0;i<n;++i)
+        {
+            sum=sum+mat[i][i]+mat[i][n-i-1];
+        }
+
+        if(n%2) 
+            sum=sum-mat[n/2][n/2];
+        
+        return sum;
+    }
+};
